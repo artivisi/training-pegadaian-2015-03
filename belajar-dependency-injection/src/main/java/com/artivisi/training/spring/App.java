e640688d4719c9e99d34426f26d82e28ede483dd
Endy Muhardin <endy.muhardin@gmail.com>
2015-03-13 10:10:48 +0700

    dependency injection manual

--- belajar-dependency-injection/src/main/java/com/artivisi/training/spring/App.java
+++ belajar-dependency-injection/src/main/java/com/artivisi/training/spring/App.java
@@ -1,13 +1,45 @@
 package com.artivisi.training.spring;
 
+import com.artivisi.training.spring.dao.ProdukDao;
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
+public class App {
+    private static final String dbDriver = "com.mysql.jdbc.Driver";
+    private static final String dbUrl = "jdbc:mysql://localhost/belajar";
+    private static final String dbUsername = "root";
+    private static final String dbPassword = "admin";
+    
+    private static Connection connectKeDb() {
+        try {
+            Class.forName(dbDriver);
+            Connection c = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+            return c;
+        } catch (ClassNotFoundException ex) {
+            Logger.getLogger(ProdukDao.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (SQLException ex) {
+            Logger.getLogger(ProdukDao.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return null;
+    }
+    
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        
+        Produk p = new Produk();
+        p.setKode("P-001");
+        p.setNama("Produk 001");
+        p.setHarga(new BigDecimal("150.000,00"));
+        
+        ProdukDao pd = new ProdukDao(connectKeDb());
+        pd.simpan(p);
     }
 }
--- belajar-dependency-injection/src/main/java/com/artivisi/training/spring/dao/ProdukDao.java
+++ belajar-dependency-injection/src/main/java/com/artivisi/training/spring/dao/ProdukDao.java
@@ -2,7 +2,6 @@ package com.artivisi.training.spring.dao;
 
 import com.artivisi.training.spring.Produk;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.ArrayList;
@@ -13,51 +12,37 @@ import java.util.logging.Logger;
 public class ProdukDao {
     
     private static final String SQL_SIMPAN = "insert into produk (kode, nama, harga) values (?,?,?)";
-    
-    private final String dbDriver = "com.mysql.jdbc.Driver";
-    private final String dbUrl = "jdbc:mysql://localhost/belajar";
-    private final String dbUsername = "root";
-    private final String dbPassword = "admin";
-    
-    private Connection connectKeDb() {
-        try {
-            Class.forName(dbDriver);
-            Connection c = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
-            return c;
-        } catch (ClassNotFoundException ex) {
-            Logger.getLogger(ProdukDao.class.getName()).log(Level.SEVERE, null, ex);
-        } catch (SQLException ex) {
-            Logger.getLogger(ProdukDao.class.getName()).log(Level.SEVERE, null, ex);
-        }
-        return null;
+    private Connection koneksi;
+
+    // constructor injection
+    public ProdukDao(Connection koneksi) {
+        this.koneksi = koneksi;
+    }
+
+    // setter injection
+    public void setKoneksi(Connection koneksi) {
+        this.koneksi = koneksi;
     }
     
     public void simpan(Produk p){
-        Connection c = connectKeDb();
         try {
-            c.setAutoCommit(false);
+            koneksi.setAutoCommit(false);
             
-            PreparedStatement ps = c.prepareStatement(SQL_SIMPAN);
+            PreparedStatement ps = koneksi.prepareStatement(SQL_SIMPAN);
             ps.setString(1, p.getKode());
             ps.setString(2, p.getNama());
             ps.setBigDecimal(3, p.getHarga());
             ps.executeUpdate();
-            c.commit();
+            koneksi.commit();
             
         } catch (SQLException ex) {
             try {
-                c.rollback();
+                koneksi.rollback();
             } catch (SQLException ex1) {
                 Logger.getLogger(ProdukDao.class.getName()).log(Level.SEVERE, null, ex1);
             }
             Logger.getLogger(ProdukDao.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            try {
-                c.close();
-            } catch (SQLException ex) {
-                Logger.getLogger(ProdukDao.class.getName()).log(Level.SEVERE, null, ex);
-            }
-        }
+        } 
     }
     
     public List<Produk> cariSemuaProduk(){
