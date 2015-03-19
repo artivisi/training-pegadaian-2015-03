5840777f863938af8f545cda69c9cff2142d667f
Endy Muhardin <endy.muhardin@gmail.com>
2015-03-19 14:13:06 +0700

    enable basic auth

--- bpjs-rest-client/src/main/java/id/co/pegadaian/simulator/bpjs/rest/client/BpjsRestClient.java
+++ bpjs-rest-client/src/main/java/id/co/pegadaian/simulator/bpjs/rest/client/BpjsRestClient.java
@@ -6,7 +6,11 @@ import id.co.pegadaian.simulator.bpjs.rest.client.dto.Tagihan;
 import java.net.URI;
 import java.util.Arrays;
 import java.util.List;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.Base64Utils;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
@@ -14,15 +18,21 @@ public class BpjsRestClient {
     private String server = "http://localhost:10000";
     RestTemplate restClient = new RestTemplate();
     
+    private HttpHeaders createAuthenticationHeader(){
+        String userpass = "user:test123";
+        String base64encoded = Base64Utils.encodeToString(userpass.getBytes());
+        HttpHeaders header = new HttpHeaders();
+        header.add("Authorization", "Basic "+base64encoded);
+        return header;
+    }
+    
+    
     public List<Tagihan> inquiry(String idpel) throws RestClientException {
         String url = "/inquiry/kesehatan/";
         
-        // mengambil response berupa string
-        String hasil = restClient.getForObject(server + url + idpel, String.class);
-        System.out.println("Hasil : "+hasil);
-        
         // mengambil response berupa map
-        ResponseEntity<Tagihan[]> hasil2 = restClient.getForEntity(server + url + idpel, Tagihan[].class);
+        HttpEntity<String> request = new HttpEntity<>(createAuthenticationHeader());
+        ResponseEntity<Tagihan[]> hasil2 = restClient.exchange(server+url+idpel, HttpMethod.GET, request, Tagihan[].class);
         Tagihan[] data = hasil2.getBody();
         
         return Arrays.asList(data);
@@ -36,8 +46,12 @@ public class BpjsRestClient {
         
         String url = "/payment/kesehatan";
         
-        URI lokasi = restClient.postForLocation(server + url, payReq);
-        PaymentResponse resp = restClient.getForObject(lokasi, PaymentResponse.class);
-        return resp;
+        HttpEntity<PaymentRequest> request = new HttpEntity<>(payReq, createAuthenticationHeader());
+        URI lokasi = restClient.postForLocation(server + url, request);
+        
+        
+        HttpEntity<String> paymentResultRequest = new HttpEntity<>(createAuthenticationHeader());
+        ResponseEntity<PaymentResponse> resp = restClient.exchange(lokasi, HttpMethod.GET, paymentResultRequest, PaymentResponse.class);
+        return resp.getBody();
     }
 }
