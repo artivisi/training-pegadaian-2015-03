fe9d76439afc8f13ee17022bb28ac0192d63c937
Endy Muhardin <endy.muhardin@gmail.com>
2015-03-23 11:02:34 +0700

    integrate bpjs-common

---
+++ bpjs-iso8583-server/src/main/java/id/co/pegadaian/simulator/BpjsConfig.java
@@ -0,0 +1,8 @@
+package id.co.pegadaian.simulator;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpjsConfig {
+    
+}
--- bpjs-iso8583-server/src/main/java/id/co/pegadaian/simulator/jpos/listener/BpjsListener.java
+++ bpjs-iso8583-server/src/main/java/id/co/pegadaian/simulator/jpos/listener/BpjsListener.java
@@ -1,16 +1,37 @@
 package id.co.pegadaian.simulator.jpos.listener;
 
+import id.co.pegadaian.simulator.BpjsConfig;
+import id.co.pegadaian.simulator.entity.Tagihan;
+import id.co.pegadaian.simulator.service.BpjsService;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.ISORequestListener;
 import org.jpos.iso.ISOSource;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 
 public class BpjsListener implements ISORequestListener {
 
+    private BpjsService bpjsService;
+    
+    public BpjsListener() {
+        System.out.println("========== Menginstankan BpjsListener ===========");
+        
+        ApplicationContext ctx = SpringApplication.run(BpjsConfig.class);
+        bpjsService = ctx.getBean(BpjsService.class);
+        
+        System.out.println("========== Menginstankan BpjsListener ===========");
+    }
+
     @Override
     public boolean process(ISOSource isoSrc, ISOMsg isoMsg) {
+        // coba untuk memanggil bpjs service
+        
+        List<Tagihan> dataTagihan = bpjsService.cariTagihan("1234567890");
+        System.out.println("Jumlah Tagihan : "+dataTagihan.size());
+        
         try {
             if (isoMsg.getMTI().equals("0800")) {
                 ISOMsg reply = (ISOMsg) isoMsg.clone();
