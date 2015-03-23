474c2d50fc7adbd5f2f3bf7005e646cf0c1c332a
Endy Muhardin <endy.muhardin@gmail.com>
2015-03-23 11:18:15 +0700

    implement inquiry request

--- bpjs-iso8583-client/src/main/java/id/co/pegadaian/simulator/App.java
+++ bpjs-iso8583-client/src/main/java/id/co/pegadaian/simulator/App.java
@@ -1,7 +1,9 @@
 package id.co.pegadaian.simulator;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import org.jpos.iso.ISOException;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.channel.ASCIIChannel;
 import org.jpos.iso.packager.ISO87APackager;
@@ -9,9 +11,28 @@ import org.jpos.iso.packager.ISO87APackager;
 public class App {
     public static void main( String[] args ) throws Exception {
         ASCIIChannel channel = new ASCIIChannel("localhost", 12345, new ISO87APackager());
-        
         SimpleDateFormat formatterWaktu = new SimpleDateFormat("MMddHHmmss");
         
+        echoTest(formatterWaktu, channel);
+        
+        inquiryTest(formatterWaktu, channel);
+    }
+
+    private static void inquiryTest(SimpleDateFormat formatterWaktu, ASCIIChannel channel) throws ISOException, IOException {
+        ISOMsg inquiryRequest = new ISOMsg("0200");
+        inquiryRequest.set(7, formatterWaktu.format(new Date()));
+        inquiryRequest.set(11, "123456");
+        inquiryRequest.set(102, "1234567890");
+        
+        channel.connect();
+        channel.send(inquiryRequest);
+        ISOMsg response = channel.receive();
+        channel.disconnect();
+        
+        System.out.println("Response : "+new String(response.pack()));
+    }
+
+    private static void echoTest(SimpleDateFormat formatterWaktu, ASCIIChannel channel) throws IOException, ISOException {
         ISOMsg echoRequest = new ISOMsg("0800");
         echoRequest.set(7, formatterWaktu.format(new Date()));
         
@@ -19,5 +40,6 @@ public class App {
         channel.send(echoRequest);
         ISOMsg response = channel.receive();
         System.out.println("Response : "+new String(response.pack()));
+        channel.disconnect();
     }
 }
--- bpjs-iso8583-server/src/main/java/id/co/pegadaian/simulator/jpos/listener/BpjsListener.java
+++ bpjs-iso8583-server/src/main/java/id/co/pegadaian/simulator/jpos/listener/BpjsListener.java
@@ -3,19 +3,25 @@ package id.co.pegadaian.simulator.jpos.listener;
 import id.co.pegadaian.simulator.BpjsConfig;
 import id.co.pegadaian.simulator.entity.Tagihan;
 import id.co.pegadaian.simulator.service.BpjsService;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.text.SimpleDateFormat;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.jpos.iso.ISOMsg;
 import org.jpos.iso.ISORequestListener;
 import org.jpos.iso.ISOSource;
+import org.jpos.iso.ISOUtil;
 import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 
 public class BpjsListener implements ISORequestListener {
 
     private BpjsService bpjsService;
-    
+    private static final SimpleDateFormat formatterBlth = new SimpleDateFormat("yyyyMM");
+    private static final SimpleDateFormat formatterJatuhTempo = new SimpleDateFormat("yyyyMMdd");
+            
     public BpjsListener() {
         System.out.println("========== Menginstankan BpjsListener ===========");
         
@@ -27,16 +33,40 @@ public class BpjsListener implements ISORequestListener {
 
     @Override
     public boolean process(ISOSource isoSrc, ISOMsg isoMsg) {
-        // coba untuk memanggil bpjs service
-        
-        List<Tagihan> dataTagihan = bpjsService.cariTagihan("1234567890");
-        System.out.println("Jumlah Tagihan : "+dataTagihan.size());
         
         try {
             if (isoMsg.getMTI().equals("0800")) {
                 ISOMsg reply = (ISOMsg) isoMsg.clone();
                 reply.setResponseMTI();
                 reply.set(39, "00");
+                isoSrc.send(reply);
+                return true;
+            }
+            
+            if(isoMsg.getMTI().equals("0200")) {
+                String idPel = isoMsg.getString(102);
+                List<Tagihan> dataTagihan = bpjsService.cariTagihan(idPel);
+                StringBuilder output = new StringBuilder();
+                BigDecimal totalAmount = BigDecimal.ZERO;
+                for (Tagihan tag : dataTagihan) {
+                    totalAmount = totalAmount.add(tag.getNilai());
+                    
+                    String nama = tag.getPeserta().getNama();
+                    String blth = formatterBlth.format(tag.getTanggalTagihan());
+                    String jatuhTempo = formatterJatuhTempo.format(tag.getTanggalJatuhTempo());
+                    String nilai = tag.getNilai().setScale(0, RoundingMode.HALF_EVEN).toPlainString();
+                    
+                    output.append(ISOUtil.padright(nama, 50, ' '));
+                    output.append(blth);
+                    output.append(jatuhTempo);
+                    output.append(ISOUtil.padleft(nilai, 12, '0'));
+                }
+                
+                ISOMsg reply = (ISOMsg) isoMsg.clone();
+                reply.setResponseMTI();
+                reply.set(4, totalAmount.setScale(0, RoundingMode.HALF_EVEN).toPlainString());
+                reply.set(39, "00");
+                reply.set(48, output.toString());
                 isoSrc.send(reply);
                 return true;
             }
