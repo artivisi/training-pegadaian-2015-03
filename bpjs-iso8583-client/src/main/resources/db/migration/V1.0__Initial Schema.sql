7d5e73f449b445676bf116c5c40cef3f1c0164f9
Endy Muhardin <endy.muhardin@gmail.com>
2015-03-23 16:45:06 +0700

    tambahan field nama

--- bpjs-iso8583-client/src/main/resources/db/migration/V1.0__Initial Schema.sql
+++ bpjs-iso8583-client/src/main/resources/db/migration/V1.0__Initial Schema.sql
@@ -2,6 +2,7 @@ create table inquiry_bpjs (
 
     id VARCHAR(36),
     idpel VARCHAR(12) NOT NULL,
+    nama VARCHAR(255),
     tanggal_tagihan DATE,
     tanggal_jatuh_tempo DATE,
     nilai DECIMAL(19,2),
