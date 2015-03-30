178b4105264f8ade30cab35cb1dae559fd5c8b11
Endy Muhardin <endy.muhardin@gmail.com>
2015-03-30 14:29:57 +0700

    mengakses rest service

--- bpjs-rest/src/main/webapp/js/aplikasi.js
+++ bpjs-rest/src/main/webapp/js/aplikasi.js
@@ -1,9 +1,27 @@
 var myApp = angular.module('aplikasiPaymentPoint', []);
 
-myApp.controller('InquiryController', ['$scope', function ($scope) {
+myApp.controller('InquiryController', ['$scope','$http', function ($scope, $http) {
         $scope.greeting = 'Hola!';
         $scope.daftarJenisProduk = [
             {id : "01", nama : "BPJS Kesehatan"},
             {id : "02", nama : "BPJS Tenaga Kerja"}
         ];
+        
+        $scope.daftarTagihan = [];
+        
+        $scope.tombolCekDiklik = function(){
+            alert("Inquiry untuk idpel "+$scope.idpel);
+            $http.get('/inquiry/kesehatan/'+$scope.idpel)
+                    .success(function(data, status, headers, config){
+                        console.log("Status : "+status);
+                        console.log("Data : ");
+                        console.log(data);
+                        $scope.daftarTagihan = data;
+                    })
+                    .error(function(data, status, headers, config){
+                        console.log("Status : "+status);
+                        console.log("Data : ");
+                        console.log(data);
+                    });
+        }
     }]);
